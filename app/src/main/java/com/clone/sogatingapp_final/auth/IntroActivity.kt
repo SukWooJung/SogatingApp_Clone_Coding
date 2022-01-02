5c0b169961190f12d53b787ce9b7fb87495c2792
정석우 <ggp03016@naver.com>
2022-01-02 17:59:31 +0900

    [Add] IntroActivity 로그인과 회원가입 버튼을 가짐

---
+++ app/src/main/java/com/clone/sogatingapp_final/auth/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.clone.sogatingapp_final.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.clone.sogatingapp_final.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
