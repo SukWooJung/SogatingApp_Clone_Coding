beeb4b1004b0e3adc53ad2a122e43fa8a1e04e01
정석우 <ggp03016@naver.com>
2022-01-03 11:23:42 +0900

    [Add] 로그아웃 기능 구현

--- app/src/main/java/com/clone/sogatingapp_final/MainActivity.kt
+++ app/src/main/java/com/clone/sogatingapp_final/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.clone.sogatingapp_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import com.clone.sogatingapp_final.auth.IntroActivity
 import com.clone.sogatingapp_final.slider.CardStackAdapter
+import com.clone.sogatingapp_final.utils.FirebaseAuthUtils
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
 import com.yuyakaido.android.cardstackview.CardStackListener
 import com.yuyakaido.android.cardstackview.CardStackView
@@ -18,6 +25,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 로그아웃 버튼
+        val logoutBtn = findViewById<Button>(R.id.logoutBtn)
+        logoutBtn.setOnClickListener{
+            Firebase.auth.signOut()
+            val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        // 카드 스택뷰
         val cardStackView = findViewById<CardStackView>(R.id.cardSTackView)
 
         manager = CardStackLayoutManager(baseContext, object : CardStackListener{
